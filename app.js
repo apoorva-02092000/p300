2cd51ec33bcf5a537c94d65b4856bf63b3854af0
apoorva-02092000 <73773023+apoorva-02092000@users.noreply.github.com>
2021-07-15 00:15:51 +0530

    new

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 console.log("test")
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.static('javas'));
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index');
 });
 
 
