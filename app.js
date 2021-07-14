43a9e24737ff97208967385492c5a1a7db822755
apoorva-02092000 <73773023+apoorva-02092000@users.noreply.github.com>
2021-07-14 21:15:25 +0530

    no

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.log("test")
 
 app.use(express.static('public'));
 app.use(express.static('javas'));
+app.use(express.static(__dirname+"/client/build"));
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
